907433a05dff88db97ed8693d62e3da9cd01ba95
Maksim Rakitin <mrakitin@bnl.gov>
2020-08-27 15:42:31 -0400

    Default logging configs

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,9 +1,8 @@
 from nslsii import configure_base
 from IPython import get_ipython
 
-configure_base(get_ipython().user_ns, 'local',
-               configure_logging=False,
-               ipython_exc_logging=False)
+configure_base(get_ipython().user_ns, 'local')
+
 bec.disable_plots()
 
 # Optional: set any metadata that rarely changes.
