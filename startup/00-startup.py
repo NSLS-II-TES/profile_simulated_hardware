82d381a4dcd4c5bc22d9f0803bcb1281cf4e2636
AbbyGiles <agiles@bnl.gov>
2020-08-28 16:40:38 -0400

    Added configure_base args back

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,8 +1,9 @@
 from nslsii import configure_base
 from IPython import get_ipython
 
-configure_base(get_ipython().user_ns, 'local')
-
+configure_base(get_ipython().user_ns, 'local',
+               configure_logging=False,
+               ipython_exc_logging=False)
 bec.disable_plots()
 
 # Optional: set any metadata that rarely changes.
