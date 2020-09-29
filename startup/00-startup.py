f85d848f800de295f5bf367dc4fb6c6dff29c28a
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-29 15:43:28 -0400

    Add try/except for configure_base

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,7 +1,14 @@
 from nslsii import configure_base
 from IPython import get_ipython
 
-configure_base(get_ipython().user_ns, 'local')
+try:
+    configure_base(get_ipython().user_ns, 'local')
+except FileNotFoundError:
+    # This is needed for older nslsii.
+    configure_base(get_ipython().user_ns, 'local',
+                   configure_logging=False,
+                   ipython_exec_logging=False)
+
 bec.disable_plots()
 
 # Optional: set any metadata that rarely changes.
