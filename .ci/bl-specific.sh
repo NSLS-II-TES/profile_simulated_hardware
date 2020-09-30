99c016d28f432ce64b19f54910a87d4195ea7164
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-30 14:34:01 -0400

    Correct conditions

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -2,7 +2,7 @@
 
 export USE_EPICS_IOC=0
 
-if [ ! -z "${USE_EPICS_IOC}" ]; then
+if [ ! -z "${USE_EPICS_IOC}" -a "${USE_EPICS_IOC}" -ne 0 ]; then
     docker run -dt --rm --name motorsim --network=host -e "PREFIX=IOC" europeanspallationsource/motorsim
 
     sleep 10
