3b5c236d80fbe467cf4cd259f1d5ce53d44ee479
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-30 12:39:29 -0400

    Cleanup

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 export USE_EPICS_IOC=1
-echo "##vso[task.setvariable variable=USE_EPICS_IOC]${USE_EPICS_IOC}"
 
 docker run -dt --rm --name motorsim --network=host -e "PREFIX=IOC" europeanspallationsource/motorsim
 
