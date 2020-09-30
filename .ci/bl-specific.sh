0b4200ecffeff15e735630ed251777970efaf905
Maksim Rakitin <mrakitin@bnl.gov>
2020-09-30 16:06:08 -0400

    Check env inside the script

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -2,6 +2,11 @@
 
 export USE_EPICS_IOC=1
 
+echo -e "env inside $0"
+echo -e "========================================================"
+env | sort -u
+echo -e "========================================================"
+
 if [ ! -z "${USE_EPICS_IOC}" -a "${USE_EPICS_IOC}" -ne 0 ]; then
     docker run -dt --rm --name motorsim --network=host -e "PREFIX=IOC" europeanspallationsource/motorsim
 
