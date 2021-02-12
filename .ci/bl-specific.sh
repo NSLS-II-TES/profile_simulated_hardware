d777037e4a30ab7e0274663c8d0d8f5d7eecce48
Maksim Rakitin <mrakitin@bnl.gov>
2021-02-12 18:35:53 -0500

    Use caproto-get instead of caget

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-caget_exists=$(which caget || echo "")
-
-if [ -z "${caget_exists}" ]; then
-    echo "caget does not exist. Trying to install it..."
-    conda install -p $HOME/miniconda/envs/${CONDA_ENV_NAME} -c nsls2forge -y epics-base
-fi
-
 export USE_EPICS_IOC=1
 
 echo -e "env inside $0"
@@ -22,7 +15,7 @@ if [ ! -z "${USE_EPICS_IOC}" -a "${USE_EPICS_IOC}" -ne 0 ]; then
     docker images
     docker ps -a
 
-    caget IOC:m2
+    caproto-get -vvvv IOC:m2
 else
     echo "Falling back to use caproto-spoof-beamline IOC"
 fi
